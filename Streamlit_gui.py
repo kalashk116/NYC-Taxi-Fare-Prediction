7f317f6bfb4f022d9d679e5e17f097701e557780
Kalash Kondekar <98111863+kalashk116@users.noreply.github.com>
2025-02-10 14:34:06 +0530

    Add files via upload

---
+++ Streamlit_gui.py
@@ -0,0 +1,423 @@
+locations = {
+    1: "Newark Airport, EWR",
+    2: "Jamaica Bay, Boro Zone, Queens",
+    3: "Allerton/Pelham Gardens, Boro Zone, Bronx",
+    4: "Alphabet City, Yellow Zone, Manhattan",
+    5: "Arden Heights, Boro Zone, Staten Island",
+    6: "Arrochar/Fort Wadsworth, Boro Zone, Staten Island",
+    7: "Astoria, Boro Zone, Queens",
+    8: "Astoria Park, Boro Zone, Queens",
+    9: "Auburndale, Boro Zone, Queens",
+    10: "Baisley Park, Boro Zone, Queens",
+    11: "Bath Beach, Boro Zone, Brooklyn",
+    12: "Battery Park, Yellow Zone, Manhattan",
+    13: "Battery Park City, Yellow Zone, Manhattan",
+    14: "Bay Ridge, Boro Zone, Brooklyn",
+    15: "Bay Terrace/Fort Totten, Boro Zone, Queens",
+    16: "Bayside, Boro Zone, Queens",
+    17: "Bedford, Boro Zone, Brooklyn",
+    18: "Bedford Park, Boro Zone, Bronx",
+    19: "Bellerose, Boro Zone, Queens",
+    20: "Belmont, Boro Zone, Bronx",
+    21: "Bensonhurst East, Boro Zone, Brooklyn",
+    22: "Bensonhurst West, Boro Zone, Brooklyn",
+    23: "Bloomfield/Emerson Hill, Boro Zone, Staten Island",
+    24: "Bloomingdale, Yellow Zone, Manhattan",
+    25: "Boerum Hill, Boro Zone, Brooklyn",
+    26: "Borough Park, Boro Zone, Brooklyn",
+    27: "Breezy Point/Fort Tilden/Riis Beach, Boro Zone, Queens",
+    28: "Briarwood/Jamaica Hills, Boro Zone, Queens",
+    29: "Brighton Beach, Boro Zone, Brooklyn",
+    30: "Broad Channel, Boro Zone, Queens",
+    31: "Bronx Park, Boro Zone, Bronx",
+    32: "Bronxdale, Boro Zone, Bronx",
+    33: "Brooklyn Heights, Boro Zone, Brooklyn",
+    34: "Brooklyn Navy Yard, Boro Zone, Brooklyn",
+    35: "Brownsville, Boro Zone, Brooklyn",
+    36: "Bushwick North, Boro Zone, Brooklyn",
+    37: "Bushwick South, Boro Zone, Brooklyn",
+    38: "Cambria Heights, Boro Zone, Queens",
+    39: "Canarsie, Boro Zone, Brooklyn",
+    40: "Carroll Gardens, Boro Zone, Brooklyn",
+    41: "Central Harlem, Boro Zone, Manhattan",
+    42: "Central Harlem North, Boro Zone, Manhattan",
+    43: "Central Park, Yellow Zone, Manhattan",
+    44: "Charleston/Tottenville, Boro Zone, Staten Island",
+    45: "Chinatown, Yellow Zone, Manhattan",
+    46: "City Island, Boro Zone, Bronx",
+    47: "Claremont/Bathgate, Boro Zone, Bronx",
+    48: "Clinton East, Yellow Zone, Manhattan",
+    49: "Clinton Hill, Boro Zone, Brooklyn",
+    50: "Clinton West, Yellow Zone, Manhattan",
+    51: "Co-Op City, Boro Zone, Bronx",
+    52: "Cobble Hill, Boro Zone, Brooklyn",
+    53: "College Point, Boro Zone, Queens",
+    54: "Columbia Street, Boro Zone, Brooklyn",
+    55: "Coney Island, Boro Zone, Brooklyn",
+    56: "Corona, Boro Zone, Queens",
+    57: "Corona, Boro Zone, Queens",
+    58: "Country Club, Boro Zone, Bronx",
+    59: "Crotona Park, Boro Zone, Bronx",
+    60: "Crotona Park East, Boro Zone, Bronx",
+    61: "Crown Heights North, Boro Zone, Brooklyn",
+    62: "Crown Heights South, Boro Zone, Brooklyn",
+    63: "Cypress Hills, Boro Zone, Brooklyn",
+    64: "Douglaston, Boro Zone, Queens",
+    65: "Downtown Brooklyn/MetroTech, Boro Zone, Brooklyn",
+    66: "DUMBO/Vinegar Hill, Boro Zone, Brooklyn",
+    67: "Dyker Heights, Boro Zone, Brooklyn",
+    68: "East Chelsea, Yellow Zone, Manhattan",
+    69: "East Concourse/Concourse Village, Boro Zone, Bronx",
+    70: "East Elmhurst, Boro Zone, Queens",
+    71: "East Flatbush/Farragut, Boro Zone, Brooklyn",
+    72: "East Flatbush/Remsen Village, Boro Zone, Brooklyn",
+    73: "East Flushing, Boro Zone, Queens",
+    74: "East Harlem North, Boro Zone, Manhattan",
+    75: "East Harlem South, Boro Zone, Manhattan",
+    76: "East New York, Boro Zone, Brooklyn",
+    77: "East New York/Pennsylvania Avenue, Boro Zone, Brooklyn",
+    78: "East Tremont, Boro Zone, Bronx",
+    79: "East Village, Yellow Zone, Manhattan",
+    80: "East Williamsburg, Boro Zone, Brooklyn",
+    81: "Eastchester, Boro Zone, Bronx",
+    82: "Elmhurst, Boro Zone, Queens",
+    83: "Elmhurst/Maspeth, Boro Zone, Queens",
+    84: "Eltingville/Annadale/Prince's Bay, Boro Zone, Staten Island",
+    85: "Erasmus, Boro Zone, Brooklyn",
+    86: "Far Rockaway, Boro Zone, Queens",
+    87: "Financial District North, Yellow Zone, Manhattan",
+    88: "Financial District South, Yellow Zone, Manhattan",
+    89: "Flatbush/Ditmas Park, Boro Zone, Brooklyn",
+    90: "Flatiron, Yellow Zone, Manhattan",
+    91: "Flatlands, Boro Zone, Brooklyn",
+    92: "Flushing, Boro Zone, Queens",
+    93: "Flushing Meadows-Corona Park, Boro Zone, Queens",
+    94: "Fordham South, Boro Zone, Bronx",
+    95: "Forest Hills, Boro Zone, Queens",
+    96: "Forest Park/Highland Park, Boro Zone, Queens",
+    97: "Fort Greene, Boro Zone, Brooklyn",
+    98: "Fresh Meadows, Boro Zone, Queens",
+    99: "Freshkills Park, Boro Zone, Staten Island",
+    100: "Garment District, Yellow Zone, Manhattan",
+    101: "Glen Oaks, Boro Zone, Queens",
+    102: "Glendale, Boro Zone, Queens",
+    103: "Governor's Island/Ellis Island/Liberty Island, Yellow Zone, Manhattan",
+    104: "Governor's Island/Ellis Island/Liberty Island, Yellow Zone, Manhattan",
+    105: "Governor's Island/Ellis Island/Liberty Island, Yellow Zone, Manhattan",
+    106: "Gowanus, Boro Zone, Brooklyn",
+    107: "Gramercy, Yellow Zone, Manhattan",
+    108: "Gravesend, Boro Zone, Brooklyn",
+    109: "Great Kills, Boro Zone, Staten Island",
+    110: "Great Kills Park, Boro Zone, Staten Island",
+    111: "Green-Wood Cemetery, Boro Zone, Brooklyn",
+    112: "Greenpoint, Boro Zone, Brooklyn",
+    113: "Greenwich Village North, Yellow Zone, Manhattan",
+    114: "Greenwich Village South, Yellow Zone, Manhattan",
+    115: "Grymes Hill/Clifton, Boro Zone, Staten Island",
+    116: "Hamilton Heights, Boro Zone, Manhattan",
+    117: "Hammels/Arverne, Boro Zone, Queens",
+    118: "Heartland Village/Todt Hill, Boro Zone, Staten Island",
+    119: "Highbridge, Boro Zone, Bronx",
+    120: "Highbridge Park, Boro Zone, Manhattan",
+    121: "Hillcrest/Pomonok, Boro Zone, Queens",
+    122: "Hollis, Boro Zone, Queens",
+    123: "Homecrest, Boro Zone, Brooklyn",
+    124: "Howard Beach, Boro Zone, Queens",
+    125: "Hudson Sq, Yellow Zone, Manhattan",
+    126: "Hunts Point, Boro Zone, Bronx",
+    127: "Inwood, Boro Zone, Manhattan",
+    128: "Inwood Hill Park, Boro Zone, Manhattan",
+    129: "Jackson Heights, Boro Zone, Queens",
+    130: "Jamaica, Boro Zone, Queens",
+    131: "Jamaica Estates, Boro Zone, Queens",
+    132: "JFK Airport, Airports, Queens",
+    133: "Kensington, Boro Zone, Brooklyn",
+    134: "Kew Gardens, Boro Zone, Queens",
+    135: "Kew Gardens Hills, Boro Zone, Queens",
+    136: "Kingsbridge Heights, Boro Zone, Bronx",
+    137: "Kips Bay, Yellow Zone, Manhattan",
+    138: "LaGuardia Airport, Airports, Queens",
+    139: "Laurelton, Boro Zone, Queens",
+    140: "Lenox Hill East, Yellow Zone, Manhattan",
+    141: "Lenox Hill West, Yellow Zone, Manhattan",
+    142: "Lincoln Square East, Yellow Zone, Manhattan",
+    143: "Lincoln Square West, Yellow Zone, Manhattan",
+    144: "Little Italy/NoLiTa, Yellow Zone, Manhattan",
+    145: "Long Island City/Hunters Point, Boro Zone, Queens",
+    146: "Long Island City/Queens Plaza, Boro Zone, Queens",
+    147: "Longwood, Boro Zone, Bronx",
+    148: "Lower East Side, Yellow Zone, Manhattan",
+    149: "Madison, Boro Zone, Brooklyn",
+    150: "Manhattan Beach, Boro Zone, Brooklyn",
+    151: "Manhattan Valley, Yellow Zone, Manhattan",
+    152: "Manhattanville, Boro Zone, Manhattan",
+    153: "Marble Hill, Boro Zone, Manhattan",
+    154: "Marine Park/Floyd Bennett Field, Boro Zone, Brooklyn",
+    155: "Marine Park/Mill Basin, Boro Zone, Brooklyn",
+    156: "Mariners Harbor, Boro Zone, Staten Island",
+    157: "Maspeth, Boro Zone, Queens",
+    158: "Meatpacking/West Village West, Yellow Zone, Manhattan",
+    159: "Melrose South, Boro Zone, Bronx",
+    160: "Middle Village, Boro Zone, Queens",
+    161: "Midtown Center, Yellow Zone, Manhattan",
+    162: "Midtown East, Yellow Zone, Manhattan",
+    163: "Midtown North, Yellow Zone, Manhattan",
+    164: "Midtown South, Yellow Zone, Manhattan",
+    165: "Midwood, Boro Zone, Brooklyn",
+    166: "Morningside Heights, Boro Zone, Manhattan",
+    167: "Morrisania/Melrose, Boro Zone, Bronx",
+    168: "Mott Haven/Port Morris, Boro Zone, Bronx",
+    169: "Mount Hope, Boro Zone, Bronx",
+    170: "Murray Hill, Yellow Zone, Manhattan",
+    171: "Murray Hill-Queens, Boro Zone, Queens",
+    172: "New Dorp/Midland Beach, Boro Zone, Staten Island",
+    173: "North Corona, Boro Zone, Queens",
+    174: "Norwood, Boro Zone, Bronx",
+    175: "Oakland Gardens, Boro Zone, Queens",
+    176: "Oakwood, Boro Zone, Staten Island",
+    177: "Ocean Hill, Boro Zone, Brooklyn",
+    178: "Ocean Parkway South, Boro Zone, Brooklyn",
+    179: "Old Astoria, Boro Zone, Queens",
+    180: "Ozone Park, Boro Zone, Queens",
+    181: "Park Slope, Boro Zone, Brooklyn",
+    182: "Parkchester, Boro Zone, Bronx",
+    183: "Pelham Bay, Boro Zone, Bronx",
+    184: "Pelham Bay Park, Boro Zone, Bronx",
+    185: "Pelham Parkway, Boro Zone, Bronx",
+    186: "Penn Station/Madison Sq West, Yellow Zone, Manhattan",
+    187: "Port Richmond, Boro Zone, Staten Island",
+    188: "Prospect-Lefferts Gardens, Boro Zone, Brooklyn",
+    189: "Prospect Heights, Boro Zone, Brooklyn",
+    190: "Prospect Park, Boro Zone, Brooklyn",
+    191: "Queens Village, Boro Zone, Queens",
+    192: "Queensboro Hill, Boro Zone, Queens",
+    193: "Queensbridge/Ravenswood, Boro Zone, Queens",
+    194: "Randalls Island, Yellow Zone, Manhattan",
+    195: "Red Hook, Boro Zone, Brooklyn",
+    196: "Rego Park, Boro Zone, Queens",
+    197: "Richmond Hill, Boro Zone, Queens",
+    198: "Ridgewood, Boro Zone, Queens",
+    199: "Rikers Island, Boro Zone, Bronx",
+    200: "Riverdale/North Riverdale/Fieldston, Boro Zone, Bronx",
+    201: "Rockaway Park, Boro Zone, Queens",
+    202: "Roosevelt Island, Boro Zone, Manhattan",
+    203: "Rosedale, Boro Zone, Queens",
+    204: "Rossville/Woodrow, Boro Zone, Staten Island",
+    205: "Saint Albans, Boro Zone, Queens",
+    206: "Saint George/New Brighton, Boro Zone, Staten Island",
+    207: "Saint Michaels Cemetery/Woodside, Boro Zone, Queens",
+    208: "Schuylerville/Edgewater Park, Boro Zone, Bronx",
+    209: "Seaport, Yellow Zone, Manhattan",
+    210: "Sheepshead Bay, Boro Zone, Brooklyn",
+    211: "SoHo, Yellow Zone, Manhattan",
+    212: "Soundview/Bruckner, Boro Zone, Bronx",
+    213: "Soundview/Castle Hill, Boro Zone, Bronx",
+    214: "South Beach/Dongan Hills, Boro Zone, Staten Island",
+    215: "South Jamaica, Boro Zone, Queens",
+    216: "South Ozone Park, Boro Zone, Queens",
+    217: "South Williamsburg, Boro Zone, Brooklyn",
+    218: "Springfield Gardens North, Boro Zone, Queens",
+    219: "Springfield Gardens South, Boro Zone, Queens",
+    220: "Spuyten Duyvil/Kingsbridge, Boro Zone, Bronx",
+    221: "Stapleton, Boro Zone, Staten Island",
+    222: "Starrett City, Boro Zone, Brooklyn",
+    223: "Steinway, Boro Zone, Queens",
+    224: "Stuy Town/Peter Cooper Village, Yellow Zone, Manhattan",
+    225: "Stuyvesant Heights, Boro Zone, Brooklyn",
+    226: "Sunnyside, Boro Zone, Queens",
+    227: "Sunset Park East, Boro Zone, Brooklyn",
+    228: "Sunset Park West, Boro Zone, Brooklyn",
+    229: "Sutton Place/Turtle Bay North, Yellow Zone, Manhattan",
+    230: "Times Sq/Theatre District, Yellow Zone, Manhattan",
+    231: "TriBeCa/Civic Center, Yellow Zone, Manhattan",
+    232: "Two Bridges/Seward Park, Yellow Zone, Manhattan",
+    233: "UN/Turtle Bay South, Yellow Zone, Manhattan",
+    234: "Union Sq, Yellow Zone, Manhattan",
+    235: "University Heights/Morris Heights, Boro Zone, Bronx",
+    236: "Upper East Side North, Yellow Zone, Manhattan",
+    237: "Upper East Side South, Yellow Zone, Manhattan",
+    238: "Upper West Side North, Yellow Zone, Manhattan",
+    239: "Upper West Side South, Yellow Zone, Manhattan",
+    240: "Van Cortlandt Park, Boro Zone, Bronx",
+    241: "Van Cortlandt Village, Boro Zone, Bronx",
+    242: "Van Nest/Morris Park, Boro Zone, Bronx",
+    243: "Washington Heights North, Boro Zone, Manhattan",
+    244: "Washington Heights South, Boro Zone, Manhattan",
+    245: "West Brighton, Boro Zone, Staten Island",
+    246: "West Chelsea/Hudson Yards, Yellow Zone, Manhattan",
+    247: "West Concourse, Boro Zone, Bronx",
+    248: "West Farms/Bronx River, Boro Zone, Bronx",
+    249: "West Village, Yellow Zone, Manhattan",
+    250: "Westchester Village/Unionport, Boro Zone, Bronx",
+    251: "Westerleigh, Boro Zone, Staten Island",
+    252: "Whitestone, Boro Zone, Queens",
+    253: "Willets Point, Boro Zone, Queens",
+    254: "Williamsbridge/Olinville, Boro Zone, Bronx",
+    255: "Williamsburg (North Side), Boro Zone, Brooklyn",
+    256: "Williamsburg (South Side), Boro Zone, Brooklyn",
+    257: "Windsor Terrace, Boro Zone, Brooklyn",
+    258: "Woodhaven, Boro Zone, Queens",
+    259: "Woodlawn/Wakefield, Boro Zone, Bronx",
+    260: "Woodside, Boro Zone, Queens",
+    261: "World Trade Center, Yellow Zone, Manhattan",
+    262: "Yorkville East, Yellow Zone, Manhattan",
+    263: "Yorkville West, Yellow Zone, Manhattan"
+}
+
+import pickle
+import streamlit as st
+import pandas as pd
+import numpy as np
+import sqlite3
+import base64
+import struct
+
+model = pickle.load(open('2020_xgboost.pkl', 'rb'), encoding='latin1')
+mode_distances_df = pd.read_csv('mode_distances.csv')
+
+average_trip_distances = {
+    (row['PULocationID'], row['DOLocationID']): row['AverageDistance']
+    for _, row in mode_distances_df.iterrows()
+}
+
+time_categories = {1: "Day", 2: "Afternoon", 3: "Night"}
+months = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
+}
+
+days = {str(i): i for i in range(1, 32)}  
+days_options = ["Select a Day"] + list(days.keys())
+
+# database connection
+conn = sqlite3.connect('taxi_fare_predictions.db')
+c = conn.cursor()
+
+# table creation
+c.execute('''
+    CREATE TABLE IF NOT EXISTS predictions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        time_of_day TEXT,
+        month INTEGER,
+        day INTEGER,
+        pickup_id INTEGER,
+        drop_id INTEGER,
+        predicted_fare REAL
+    )
+''')
+conn.commit()
+
+with st.container(key="test_container"):
+    st.markdown('<div class="container">', unsafe_allow_html=True)
+    
+    # Input values that user inserts
+    st.title("Taxi Fare Prediction")
+
+    # Button for Tableau
+    st.markdown("<a href='https://public.tableau.com/views/TaxifarePred/Dashboard1?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link' target='_blank'><button style='background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 5px; font-size: 16px; cursor: pointer;'>Open Tableau</button></a>", unsafe_allow_html=True)
+
+    time_of_day_var = st.selectbox("Select Time of Day:", ["Select Time of Day", "Day", "Afternoon", "Night"])
+    month_var = st.selectbox("Select a Month:", ["Select a Month"] + list(months.keys()))
+
+    # Day Dropdown
+    day_var = st.selectbox("Select a Day:", days_options)
+
+    # Pickup ID Entry
+    pickup_id_entry = st.number_input("Enter Pickup ID:", min_value=0)
+
+    # Drop ID Entry
+    drop_id_entry = st.number_input("Enter Drop ID:", min_value=0)
+
+    # Update Locations Button
+    def update_location_display(pickup_id, drop_id):
+        valid_location_found = False
+        pickup_location_description = ""
+        drop_location_description = ""
+
+        if pickup_id in locations:
+            pickup_location_description = f"PICKUP LOCATION: {locations[pickup_id]}"
+            valid_location_found = True
+        else:
+            pickup_location_description = "PICKUP_ID is not valid."
+
+        if drop_id in locations:
+            drop_location_description = f"DROP LOCATION: {locations[drop_id]}"
+            valid_location_found = True
+        else:
+            drop_location_description = "DROP_ID is not valid."
+
+        st.write(pickup_location_description)
+        st.write(drop_location_description)
+
+        # Calculating the average trip distance
+        if valid_location_found:
+            trip_distance = average_trip_distances.get((pickup_id, drop_id))
+            if trip_distance is not None:
+                st.write(f"AVERAGE TRIP DISTANCE: {trip_distance} miles")
+                return trip_distance 
+            else:
+                st.write("No average trip distance data available for the provided locations.")
+        else:
+            st.write("No valid locations found for the provided IDs.")
+        
+        return None 
+
+    # Button to update location information
+    if st.button("Update Locations"):
+        update_location_display(int(pickup_id_entry), int(drop_id_entry))
+
+    # Predict button click functions
+    if st.button("Predict Fare"):
+        try:
+            time_of_day = time_of_day_var
+            if time_of_day == "Select Time of Day":
+                st.error("Error: Please select the time of day.")
+            
+            month = month_var
+            if month == "Select a Month":
+                st.error("Error: Please select a valid month.")
+            
+            day = day_var
+            if day == "Select a Day":
+                st.error("Error: Please select a valid day.")
+
+            pickup_id = int(pickup_id_entry)
+            drop_id = int(drop_id_entry)
+
+            if pickup_id == drop_id:
+                st.error("PICKUP_ID and DROP_ID cannot be the same. Please enter different IDs.")
+
+            else:
+                trip_distance = update_location_display(pickup_id, drop_id)
+                if trip_distance is None:
+                    st.error("Error: No average trip distance data available for the provided locations.")
+
+                else:
+                    time_category_code = list(time_categories.keys())[list(time_categories.values()).index(time_of_day)]
+                    input_data = np.array([[time_category_code, int(days[day]), months[month], pickup_id, drop_id, trip_distance]])
+                    prediction = model.predict(input_data)
+                    prediction = struct.unpack('f', prediction)
+                    
+                    c.execute('''
+                        INSERT INTO predictions (time_of_day, month, day, pickup_id, drop_id, predicted_fare) 
+                        VALUES (?, ?, ?, ?, ?, ?)
+                    ''', (time_of_day, months[month], int(days[day]), pickup_id, drop_id, round(prediction[0],2)))
+                    conn.commit()
+
+                    st.success(f"Predicted fare: ${prediction[0]:.2f}")
+
+        except Exception as e:
+            st.error(f"Unexpected Error: {str(e)}")
+
+    conn.close()
+    st.markdown('</div>', unsafe_allow_html=True)
